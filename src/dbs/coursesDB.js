2aa00cbd3dbb86b81f4b392d611232b85841840a
Grip_Ers <87687774+Gripers@users.noreply.github.com>
2023-01-12 23:50:41 +0500

    Update coursesDB.js

--- src/dbs/coursesDB.js
+++ src/dbs/coursesDB.js
@@ -5,16 +5,8 @@ const coursesDB = [
   },
   {
     id: 2,
-    course: 'Logistika',
-  },
-  {
-    id: 3,
     course: 'Math',
-  },
-  {
-    id: 4,
-    course: 'IELTS Hardwork',
-  },
+  }
 ];
 
 export default coursesDB;
